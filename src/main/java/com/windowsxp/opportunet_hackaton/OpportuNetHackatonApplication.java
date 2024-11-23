268fbf0799d5543b9794a753732ce9be67e638fa
copemonster <alankassymbek@gmail.com>
2024-11-23 20:16:00 +0500

    Initial commit

---
+++ src/main/java/com/windowsxp/opportunet_hackaton/OpportuNetHackatonApplication.java
@@ -0,0 +1,13 @@
+package com.windowsxp.opportunet_hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpportuNetHackatonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpportuNetHackatonApplication.class, args);
+    }
+
+}
